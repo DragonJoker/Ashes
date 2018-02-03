de713a3b816e6126f968eb4377d06c3d7e209814
Sylvain Doremus <dragonjoker59@hotmail.com>
2018-02-03 13:05:06 +0100

    Loading initial states at device creation, in GlRenderer.

--- source/Renderer/GlRenderer/Src/Miscellaneous/OpenGLDefines.hpp
+++ source/Renderer/GlRenderer/Src/Miscellaneous/OpenGLDefines.hpp
@@ -206,6 +206,7 @@ namespace gl_renderer
 	using PFN_glGenTextures = void ( GLAPIENTRY * )( GLsizei n, GLuint * textures );
 	using PFN_glGenerateMipmap = void ( GLAPIENTRY * )( GLenum target );
 	using PFN_glGetError = GLenum( GLAPIENTRY * )( void );
+	using PFN_glGetFloatv = void ( GLAPIENTRY * )( GLenum pname, GLfloat * data );
 	using PFN_glGetIntegerv = void ( GLAPIENTRY * )( GLenum pname, GLint * data );
 	using PFN_glGetProgramInfoLog = void ( GLAPIENTRY * ) ( GLuint program, GLsizei bufSize, GLsizei* length, GLchar* infoLog );
 	using PFN_glGetProgramiv = void ( GLAPIENTRY * ) ( GLuint program, GLenum pname, GLint* param );
