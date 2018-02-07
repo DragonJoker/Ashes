5721d9eeb73ba819004f8bdb51fede3d5cc20efc
Sylvain Doremus <dragonjoker59@hotmail.com>
2018-02-07 10:51:24 +0100

    Removed lua files in tests folders, and using a loop to add the projects. The assets and shader files are copied in the appropriate output folders.

--- source/Test/premake5.lua
+++ source/Test/premake5.lua
@@ -1,17 +1,69 @@
 dofile "../Premake/FindwxWidgets.lua"
 
-include( "00-Common/premake5.lua" )
-include( "01-DeviceCreation/premake5.lua" )
-include( "02-SwapChainCreation/premake5.lua" )
-include( "03-VertexLayout/premake5.lua" )
-include( "04-StagingBuffer/premake5.lua" )
-include( "05-Texture2D/premake5.lua" )
-include( "06-UniformBuffers/premake5.lua" )
-include( "07-Texture1D/premake5.lua" )
-include( "08-TextureBuffer/premake5.lua" )
-include( "09-FrameBuffer/premake5.lua" )
-include( "10-SpinningCube/premake5.lua" )
-include( "11-Texture3D/premake5.lua" )
-include( "12-TextureCube/premake5.lua" )
-include( "13-PushConstants/premake5.lua" )
-include( "14-RenderToCube/premake5.lua" )
+folders = os.matchdirs( "*" )
+
+for i, folder in ipairs( folders ) do
+	if ( folder ~= "Assets" ) then
+		currentSourceDir = path.join( sourceDir, "Test", folder )
+		currentBinaryDir = path.join( binaryDir, "Test", folder )
+
+		project( folder )
+
+		location( currentBinaryDir )
+
+		if ( folder == "00-Common" ) then
+			kind( "StaticLib" )
+			targetdir( path.join( outputDir, "%{cfg.architecture}", "%{cfg.buildcfg}", staticLibDir ) )
+			includedirs{
+				path.join( sourceDir, "Core" ),
+				path.join( sourceDir, "Renderer", "Renderer", "Src" ),
+				path.join( binaryDir, "Renderer", "Renderer", "Src" ),
+				path.join( currentBinaryDir, "Src" ),
+				path.join( currentSourceDir, "Src" )
+			}
+			links{
+				"Utils",
+				"Renderer"
+			}
+			postbuildcommands {
+				"{COPY} " .. path.join( sourceDir, "Test", "Assets" ) .. " " .. path.join( outputDir, "%{cfg.architecture}", "%{cfg.buildcfg}", assetsDir, "Assets" )
+			}
+		else
+			kind( "WindowedApp" )
+			targetdir( path.join( outputDir, "%{cfg.architecture}", "%{cfg.buildcfg}", executableDir ) )
+			includedirs{
+				path.join( sourceDir, "Core" ),
+				path.join( sourceDir, "Renderer", "Renderer", "Src" ),
+				path.join( binaryDir, "Renderer", "Renderer", "Src" ),
+				path.join( sourceDir, "Test", "00-Common", "Src" ),
+				path.join( currentBinaryDir, "Src" ),
+				path.join( currentSourceDir, "Src" )
+			}
+			links{
+				"Utils",
+				"Renderer",
+				"00-Common"
+			}
+			shadersFolder = path.join( sourceDir, "Test", folder, "Shaders" )
+			if ( os.isdir( shadersFolder ) ) then
+				postbuildcommands {
+					"{COPY} " .. shadersFolder .. " " .. path.join( outputDir, "%{cfg.architecture}", "%{cfg.buildcfg}", assetsDir, folder, "Shaders" )
+				}
+			end
+		end
+
+		files{ currentSourceDir .. "/Src/**.hpp",
+			currentSourceDir .. "/Src/**.inl",
+			currentSourceDir .. "/Src/**.cpp"
+		}
+
+		vpaths{ ["Header Files"] = "**.hpp" }
+		vpaths{ ["Header Files"] = "**.inl" }
+		vpaths{ ["Source Files"] = "**.cpp" }
+
+		filter( "configurations:Debug" )
+			wx_config {Unicode="yes", Version="3.1", Libs="core,aui", Static="yes", Debug="yes"}
+		filter( "configurations:Release" )
+			wx_config {Unicode="yes", Version="3.1", Libs="core,aui", Static="yes"}
+	end
+end
