6ecfb9511f0c8929f87a514eb920a119ebd285ad
Sylvain Doremus <dragonjoker59@hotmail.com>
2018-02-14 01:34:58 +0100

    Updating billboards sample.

---
+++ source/Samples/05-Billboards/Src/NodesRenderer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Prerequisites.hpp"
+
+#include <NodesRenderer.hpp>
+
+namespace vkapp
+{
+	class NodesRenderer
+		: public common::NodesRenderer
+	{
+	public:
+		NodesRenderer( renderer::Device const & device
+			, std::string const & fragmentShaderFile
+			, std::vector< renderer::PixelFormat > const & formats
+			, bool clearViews
+			, bool opaqueNodes
+			, renderer::UniformBuffer< common::SceneData > const & matrixUbo );
+
+	private:
+		void doFillDescriptorLayoutBindings( renderer::DescriptorSetLayoutBindingArray & bindings )override;
+		void doFillDescriptorSet( renderer::DescriptorSetLayout & descriptorLayout
+			, renderer::DescriptorSet & descriptorSet )override;
+
+	private:
+		renderer::UniformBuffer< common::SceneData > const & m_sceneUbo;
+	};
+}
