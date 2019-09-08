80df5862f8804ef446b5cc3dc5c9721e633958f2
DragonJoker <dragonjoker59@hotmail.com>
2019-09-08 17:29:51 +0200

    Added headers ashespp.hpp and util.hpp.

---
+++ include/util/util.hpp
@@ -0,0 +1,11 @@
+/*
+This file belongs to Ashes.
+See LICENSE file in root folder.
+*/
+#ifndef ___ashes_util_HPP___
+#define ___ashes_util_HPP___
+#pragma once
+
+#include "util/UtilsPrerequisites"
+
+#endif
