4bd0c92cff088930028298eb8b9ae1a9aa2a0585
DragonJoker <dragonjoker59@hotmail.com>
2021-03-21 09:59:41 +0100

    GlRenderer: Added missing enums.

--- source/ashes/renderer/GlRenderer/GlRendererPch.hpp
+++ source/ashes/renderer/GlRenderer/GlRendererPch.hpp
@@ -1,3 +1,11 @@
 #include "renderer/GlRenderer/GlRendererPrerequisites.hpp"
 
-#include "renderer/GlRenderer/ashesgl_api.hpp"
+#include <ashes/common/Exception.hpp>
+#include <ashes/common/VkTypeTraits.hpp>
+
+#ifndef VK_NO_PROTOTYPES
+#	define VK_NO_PROTOTYPES
+#endif
+#include <ashes/ashes.h>
+
+#include <limits>
