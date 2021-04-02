38730d6b8aa757440dac5425e8825476d871358b
DragonJoker <dragonjoker59@hotmail.com>
2021-04-02 12:27:39 +0200

    GlRenderer: Updated memory barrier command, for image memory bindings.

--- source/ashes/renderer/GlRenderer/Command/Commands/GlMemoryBarrierCommand.cpp
+++ source/ashes/renderer/GlRenderer/Command/Commands/GlMemoryBarrierCommand.cpp
@@ -18,6 +18,70 @@ namespace ashes::gl
 			, cmd.flags );
 	}
 
+	bool bindPreBarrier( DeviceMemoryBinding const & binding
+		, VkAccessFlags srcAccessMask
+		, VkAccessFlags dstAccessMask
+		, VkDeviceMemorySet & downloads
+		, VkDeviceMemorySet & uploads
+		, CmdList & list )
+	{
+		bool result = binding.isMapped();
+
+		if ( result )
+		{
+			if ( !checkFlag( dstAccessMask, VK_ACCESS_TRANSFER_READ_BIT )
+				&& ( checkFlag( srcAccessMask, VK_ACCESS_MEMORY_WRITE_BIT )
+					|| checkFlag( srcAccessMask, VK_ACCESS_HOST_WRITE_BIT ) ) )
+			{
+				uploads.insert( binding.getParent() );
+				list.push_back( makeCmd< OpType::eUploadMemory >( binding.getParent()
+					, binding.getOffset()
+					, binding.getSize() ) );
+			}
+			else if ( checkFlag( srcAccessMask, VK_ACCESS_TRANSFER_WRITE_BIT ) )
+			{
+				downloads.insert( binding.getParent() );
+				list.push_back( makeCmd< OpType::eDownloadMemory >( binding.getParent()
+					, binding.getOffset()
+					, binding.getSize() ) );
+			}
+		}
+
+		return result;
+	}
+
+	bool bindPostBarrier( DeviceMemoryBinding const & binding
+		, VkAccessFlags srcAccessMask
+		, VkAccessFlags dstAccessMask
+		, VkDeviceMemorySet & downloads
+		, VkDeviceMemorySet & uploads
+		, CmdList & list )
+	{
+		bool result = binding.isMapped();
+
+		if ( binding.isMapped() )
+		{
+			if ( !checkFlag( srcAccessMask, VK_ACCESS_TRANSFER_WRITE_BIT )
+				&& ( checkFlag( dstAccessMask, VK_ACCESS_HOST_READ_BIT )
+					|| checkFlag( dstAccessMask, VK_ACCESS_MEMORY_READ_BIT ) ) )
+			{
+				downloads.insert( binding.getParent() );
+				list.push_back( makeCmd< OpType::eDownloadMemory >( binding.getParent()
+					, binding.getOffset()
+					, binding.getSize() ) );
+			}
+			else if ( checkFlag( dstAccessMask, VK_ACCESS_TRANSFER_READ_BIT ) )
+			{
+				uploads.insert( binding.getParent() );
+				list.push_back( makeCmd< OpType::eUploadMemory >( binding.getParent()
+					, binding.getOffset()
+					, binding.getSize() ) );
+			}
+		}
+
+		return result;
+	}
+
 	void buildMemoryBarrierCommand( VkPipelineStageFlags after
 		, VkPipelineStageFlags before
 		, VkDependencyFlags dependencyFlags
@@ -33,26 +97,22 @@ namespace ashes::gl
 
 		for ( auto & barrier : bufferMemoryBarriers )
 		{
-			if ( get( barrier.buffer )->isMapped() )
-			{
-				hasMapped = true;
-				if ( !checkFlag( barrier.dstAccessMask, VK_ACCESS_TRANSFER_READ_BIT )
-					&& ( checkFlag( barrier.srcAccessMask, VK_ACCESS_MEMORY_WRITE_BIT )
-						|| checkFlag( barrier.srcAccessMask, VK_ACCESS_HOST_WRITE_BIT ) ) )
-				{
-					uploads.insert( get( barrier.buffer )->getMemoryBinding().getParent() );
-					list.push_back( makeCmd< OpType::eUploadMemory >( get( barrier.buffer )->getMemoryBinding().getParent()
-						, get( barrier.buffer )->getMemoryBinding().getOffset()
-						, get( barrier.buffer )->getMemoryBinding().getSize() ) );
-				}
-				else if ( checkFlag( barrier.srcAccessMask, VK_ACCESS_TRANSFER_WRITE_BIT ) )
-				{
-					downloads.insert( get( barrier.buffer )->getMemoryBinding().getParent() );
-					list.push_back( makeCmd< OpType::eDownloadMemory >( get( barrier.buffer )->getMemoryBinding().getParent()
-						, get( barrier.buffer )->getMemoryBinding().getOffset()
-						, get( barrier.buffer )->getMemoryBinding().getSize() ) );
-				}
-			}
+			hasMapped = bindPreBarrier( get( barrier.buffer )->getMemoryBinding()
+				, barrier.srcAccessMask
+				, barrier.dstAccessMask
+				, downloads 
+				, uploads
+				, list ) || hasMapped;
+		}
+
+		for ( auto & barrier : imageMemoryBarriers )
+		{
+			hasMapped = bindPreBarrier( get( barrier.image )->getMemoryBinding()
+				, barrier.srcAccessMask
+				, barrier.dstAccessMask
+				, downloads
+				, uploads
+				, list ) || hasMapped;
 		}
 
 		list.push_back( makeCmd< OpType::eMemoryBarrier >( getMemoryBarrierFlags( before )
@@ -62,25 +122,22 @@ namespace ashes::gl
 
 		for ( auto & barrier : bufferMemoryBarriers )
 		{
-			if ( get( barrier.buffer )->isMapped() )
-			{
-				if ( !checkFlag( barrier.srcAccessMask, VK_ACCESS_TRANSFER_WRITE_BIT )
-					&& ( checkFlag( barrier.dstAccessMask, VK_ACCESS_HOST_READ_BIT )
-						|| checkFlag( barrier.dstAccessMask, VK_ACCESS_MEMORY_READ_BIT ) ) )
-				{
-					downloads.insert( get( barrier.buffer )->getMemoryBinding().getParent() );
-					list.push_back( makeCmd< OpType::eDownloadMemory >( get( barrier.buffer )->getMemoryBinding().getParent()
-						, get( barrier.buffer )->getMemoryBinding().getOffset()
-						, get( barrier.buffer )->getMemoryBinding().getSize() ) );
-				}
-				else if ( checkFlag( barrier.dstAccessMask, VK_ACCESS_TRANSFER_READ_BIT ) )
-				{
-					uploads.insert( get( barrier.buffer )->getMemoryBinding().getParent() );
-					list.push_back( makeCmd< OpType::eUploadMemory >( get( barrier.buffer )->getMemoryBinding().getParent()
-						, get( barrier.buffer )->getMemoryBinding().getOffset()
-						, get( barrier.buffer )->getMemoryBinding().getSize() ) );
-				}
-			}
+			bindPostBarrier( get( barrier.buffer )->getMemoryBinding()
+				, barrier.srcAccessMask
+				, barrier.dstAccessMask
+				, downloads
+				, uploads
+				, list );
+		}
+
+		for ( auto & barrier : imageMemoryBarriers )
+		{
+			bindPostBarrier( get( barrier.image )->getMemoryBinding()
+				, barrier.srcAccessMask
+				, barrier.dstAccessMask
+				, downloads
+				, uploads
+				, list );
 		}
 	}
 }
